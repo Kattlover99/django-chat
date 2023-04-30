53112752dd80a7ae608789b4d91d4cc63fbd1d14
kattlover99 <thrillseeker.nw@gmail.com>
2023-04-30 14:47:42 +0900

    update room models.py

--- room/models.py
+++ room/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
 class Room(models.Model):
-    name = models.CharField(max_length=255)
+    # name field
+    name = models.CharField(max_length=200)
+    # slug field
     slug = models.SlugField(unique=True)
